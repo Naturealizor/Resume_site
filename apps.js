7544b1fd8a1ffd895a92551d1469ea1b6ea2c77d
Naturealizor <31334963+Naturealizor@users.noreply.github.com>
2021-07-18 08:22:23 -0500

    Installed npm

--- apps.js
+++ apps.js
@@ -12,3 +12,9 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+// var http = require('http');
+//  http.createServer(function (req, res) {
+//    res.writeHead(200, {'Content-Type': 'text/html'});
+//    res.end('Hello World!');
+//  }).listen(8080);
