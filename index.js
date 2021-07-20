ad039f87c0255a9872513c7ab866875b2a766471
Naturealizor <31334963+Naturealizor@users.noreply.github.com>
2021-07-20 12:45:10 -0500

    Configuring files for npm

--- index.js
+++ index.js
@@ -13,8 +13,3 @@ server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-// var http = require('http');
-//  http.createServer(function (req, res) {
-//    res.writeHead(200, {'Content-Type': 'text/html'});
-//    res.end('Hello World!');
-//  }).listen(8080);
