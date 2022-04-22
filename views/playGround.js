897f251c648d8a4d2b1aa3f4aaeb27b2153dcaa2
Jacob Jordan <31334963+Naturealizor@users.noreply.github.com>
2022-04-21 21:59:05 -0400

    Added const declarations

--- views/playGround.js
+++ views/playGround.js
@@ -1,4 +1,11 @@
 const button = document.querySelector('input');
+const orgStart = document.getElementById('startingPoint');
+const destination = document.getElementById('destination');
+const orgMiles = document.getElementById('startingMiles');
+const endMiles = document.getElementById('endingMiles');
+const gasAmt = document.getElementById('gas');
+const buyer = document.getElementById('whoPaid');
+const misc = document.getElementById('otherExpenses');
 
 function showAdded () {
     if (button.value === 'Add Date') {
